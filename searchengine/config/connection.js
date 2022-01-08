7430abb8f648bc52a28de3a38c55f39cc323cbdb
phamthanhhang208 <xmichiyo99@gmail.com>
2022-01-09 02:45:27 +0700

    added remote elasticsearch

--- searchengine/config/connection.js
+++ searchengine/config/connection.js
@@ -1,7 +1,8 @@
 const elasticsearch = require('@elastic/elasticsearch');
 const client = new elasticsearch.Client({
-    node: 'http://localhost:9200',
-    log: 'error'
- });
+  //node: 'http://localhost:9200',
+  node: 'https://9al1wnxwvy:qyrdj8jdil@olive-688332222.us-east-1.bonsaisearch.net:443',
+  log: 'error',
+});
 
-module.exports = client
+module.exports = client;
