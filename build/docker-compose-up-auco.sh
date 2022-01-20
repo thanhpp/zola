34b1357c2306858ad04c3ab41e9342a1cc9706bd
thanhpp <thanhphanphu18@gmail.com>
2022-01-21 01:00:59 +0700

    remove dockerbuildkit=1
    Author: thanhphanphu18@gmail.com

--- build/docker-compose-up-auco.sh
+++ build/docker-compose-up-auco.sh
@@ -1,3 +1,3 @@
-export DOCKER_BUILDKIT=1
+# export DOCKER_BUILDKIT=1
 docker-compose -f docker-compose.auco.yml down
 docker-compose -f docker-compose.auco.yml up --build --force-recreate
--- build/docker-compose-up.sh
+++ build/docker-compose-up.sh
@@ -1,3 +1,3 @@
-export DOCKER_BUILDKIT=1
+# export DOCKER_BUILDKIT=1
 docker-compose down
 docker-compose up --build --force-recreate -d
